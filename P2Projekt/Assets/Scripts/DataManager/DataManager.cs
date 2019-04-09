f9ffa0ad7267dfac192c36ace856405a41e4e78e
-Andreas <andreaschristensen2010@live.dk>
2019-04-09 14:40:52 +0200

    Startet på Statistics med SaveStatistics()

---
+++ P2Projekt/Assets/Scripts/DataManager/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public void Start()
+    {
+
+    }
+    public bool SaveStatistics(Statistic stats)
+    {
+        if (stats == null)
+            return false;
+
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        
+        if (System.IO.Directory.Exists(path))
+        {
+            string data = JsonUtility.ToJson(stats);
+            System.IO.File.WriteAllText(path + @"\Noget_midlertidligt.json", data);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ P2Projekt/Assets/Scripts/Statistics/Statistic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+[Serializable]
+public class Statistic
+{
+    public int AmountOfDeadFish;
+    public float AverageHunger;
+    public float AverageStress;
+    public int TotalFish;
+    public float TotalInsertedFood;
+
+    public Statistic(int deadFish, float averageHunger, float averageStress, int totalFish, float totalInsertedFood)
+    {
+        this.AmountOfDeadFish = deadFish;
+        this.AverageHunger = averageHunger;
+        this.AverageStress = averageStress;
+        this.TotalFish = totalFish;
+        this.TotalInsertedFood = totalInsertedFood;
+    }
+}
