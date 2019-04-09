702cf151a315bc1e694c4544aa3faa30dcde95e2
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2019-04-09 13:06:02 +0200

    Mathtools startfil
    Filen til mathtools er blevet lavet

---
+++ P2Projekt/Assets/Scripts/MathTools.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathTools : MonoBehaviour
+{
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    Vector3 GetDistanceBetweenVectors(Vector3 vector1, Vector3 vector2) {
+        Vector3 newVector = Mathf.Sqrt();
+
+        return newVector(3, 4, 5)}
+}
