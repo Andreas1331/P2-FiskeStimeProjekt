b6c871500df8af80273421762e3c1c8dbc517ecc
AsBeeb <asbeeb@gmail.com>
2019-04-11 09:47:26 +0200

    Start button added
    Start button now opens a new scene.
    Runs in single-mode, closes the current scene(MainMenu) and opens a new scene

---
+++ P2Projekt/Assets/Scripts/GUI/LoatScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoatScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadBeebScene()
+    {
+        SceneManager.LoadScene("BeebsFishSCENEV1", LoadSceneMode.Single);
+    }
+}
