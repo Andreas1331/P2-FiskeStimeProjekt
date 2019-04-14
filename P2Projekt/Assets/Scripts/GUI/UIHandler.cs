2b08f9060fddc266a1c6e9033ff668ff7c5552ae
AsBeeb <asbeeb@gmail.com>
2019-04-14 11:58:56 +0200

    Pause Menu Added to SampleScene
    Added Pausemenu.
    No pause function yet though...

--- P2Projekt/Assets/Scripts/GUI/UIHandler.cs
+++ P2Projekt/Assets/Scripts/GUI/UIHandler.cs
@@ -14,8 +14,10 @@ public class UIHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Scans for escape key stroke
+        TogglePauseMenuInGame();
     }
+
     public void ApplicationQuit()
     {
 #if UNITY_EDITOR
@@ -29,4 +31,35 @@ public class UIHandler : MonoBehaviour
     {
         SceneManager.LoadScene("Main", LoadSceneMode.Single);
     }
+
+    [SerializeField] private GameObject PauseMenuUI;
+    [SerializeField] private bool isPaused;
+    public void TogglePauseMenuInGame()
+    {
+        //Toggles pause menu on esc key press
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivatePauseMenu();
+        }
+        else
+        {
+            DeactivatePauseMenu();
+        }
+    }
+
+    void ActivatePauseMenu()
+    {
+        PauseMenuUI.SetActive(true);
+    }
+    void DeactivatePauseMenu()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+
+
 }
