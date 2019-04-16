679d1c8903816a4bceddafb16bc901b976586575
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2019-04-16 10:04:01 +0200

    Camera implementeret

---
+++ P2Projekt/Assets/Scripts/CameraControlls/CameraZoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private GameObject _net;
+    public GameObject Net { set { if (value != null) _net = value; } }
+    float scale = 0.2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Net = GameObject.FindGameObjectWithTag("Net");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousewheel = Input.GetAxis("Mouse ScrollWheel") ;
+        if (mousewheel > 0f) {
+            transform.localPosition = new Vector3(0, 0, transform.localPosition.z + mousewheel);
+        }
+        else {
+            transform.localPosition = new Vector3(0, 0, transform.localPosition.z + mousewheel);
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            transform.localPosition = new Vector3(0,0, -_net.transform.lossyScale.z/2) ;
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.localPosition = new Vector3(0,0,transform.localPosition.z + 0.3f);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.localPosition = new Vector3(0, 0, transform.localPosition.z - 0.3f);
+        }
+
+        
+    }
+    void mouseScrolls() {
+        
+    }
+}
--- P2Projekt/Assets/Scripts/DataManager/DataManager.cs
+++ P2Projekt/Assets/Scripts/DataManager/DataManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DataManager : MonoBehaviour
 {
@@ -21,14 +22,14 @@ public class DataManager : MonoBehaviour
 
     public void Start()
     {
-        //Rainbowtrout rt = new Rainbowtrout(1, RainbowPreFab);
+        Rainbowtrout rt = new Rainbowtrout(1, 0.1f, RainbowPreFab);
         //Rainbowtrout rtt = new Rainbowtrout(1, RainbowPreFab);
         //rt.MoveTowards(new Vector3(0.5f, 0.2f, 0.4f));
 
         //Food firstFoodDrop = new Food(1,FoodPreFab);
 
-        AddFishToNet(5);
-        AddFoodToNet(1, 3);
+        //AddFishToNet(5);
+        //AddFoodToNet(1, 3);
     }
     private void Update()
     {
@@ -56,13 +57,13 @@ public class DataManager : MonoBehaviour
             deathCounter++;
         }
     }
+
     public void RemoveFood(Food foodToRemove)
     {
         if (foodList.Contains(foodToRemove))
         {
             foodPool.Add(foodToRemove);
-            foodList.Remove(foodToRemove);
-            
+            foodList.Remove(foodToRemove);         
         }
     }
 
@@ -70,7 +71,7 @@ public class DataManager : MonoBehaviour
     {
         for (int i = 0; i < howManyToAdd; i++) {
             fishCounter++;
-            fishList.Add(new Rainbowtrout(fishCounter, RainbowPreFab));
+            fishList.Add(new Rainbowtrout(fishCounter, 0.1f, RainbowPreFab));
         }
     }
     public void AddFoodToNet(int howManyToAdd, int amountOfFood)
@@ -81,4 +82,44 @@ public class DataManager : MonoBehaviour
             foodList.Add(new Food(foodCounter, FoodPreFab, amountOfFood));
         }
     }
+    //GUI TOOLS _______________________________________________________________START
+    #region GUI TOOLS
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("BeebMenu", LoadSceneMode.Single);
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+    public void ApplicationQuit()
+    {
+#if UNITY_EDITOR
+        //Sættes sådan at vi kan teste i Unity editoren.
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    //Method to set simulation time
+    public void SetSimSpeed(float timeFactor)
+    {
+        Time.timeScale = timeFactor;
+    }
+
+
+    public void ActivatePauseMenu(GameObject PauseMenuUI)
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+    public void DeactivatePauseMenu(GameObject PauseMenuUI)
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    #endregion
+    //GUI TOOLS _______________________________________________________________END
+
 }
