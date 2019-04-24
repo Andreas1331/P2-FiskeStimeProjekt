0ffa4172a17f2f9ce532e269abf56a23e9e5093f
Michael Lindgaard Nielsen <michni18@student.aau.dk>
2019-04-24 13:08:26 +0200

    intet er ændret

--- P2Projekt/Assets/Scripts/MathTools.cs
+++ P2Projekt/Assets/Scripts/MathTools.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+namespace MathtoolsName {
     public class MathTools
     {
         public float GetDistanceBetweenVectors(Vector3 vec1, Vector3 vec2)
@@ -19,14 +19,14 @@
             float dotProduct = (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z);
             float vec1Length = Mathf.Sqrt(Mathf.Pow(vec1.x, 2) + Mathf.Pow(vec1.y, 2) + Mathf.Pow(vec1.z, 2));
             float vec2Length = Mathf.Sqrt(Mathf.Pow(vec2.x, 2) + Mathf.Pow(vec2.y, 2) + Mathf.Pow(vec2.z, 2));
-         
+
             //Debug.Log(RadianToDegree(Mathf.Acos(dotProduct / (vec1Length * vec2Length))));
 
             return RadianToDegree(Mathf.Acos(dotProduct / (vec1Length * vec2Length)));
         }
 
         public float GetOpposingCatheter(float angle, float length) => Mathf.Sin(DegreeToRadian(angle)) * length;
-        
+
         public float RadianToDegree(float angle)
         {
             //Debug.Log("RadianToDegree: " + angle * (180 / Mathf.PI));
@@ -39,3 +39,5 @@
             return Mathf.PI * angle / 180;
         }
     }
+
+}
